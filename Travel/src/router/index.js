30e2b99427a8ddea4c7d7710f0a28891d2debcba
tracy.teng <tyh11830@126.com>
2018-12-04 16:58:29 +0800

    finish detail-banner

--- Travel/src/router/index.js
+++ Travel/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/home/Home'
 import City from '@/pages/city/City'
+import Detail from '@/pages/detail/Detail'
 
 Vue.use(Router)
 
@@ -11,8 +12,12 @@ export default new Router({
     name: 'Home',
     component: Home
    }, {
-    path: '/city',
+    path: '/city', 
     name: 'City',
     component: City
-   }]
+   }, {
+    path: '/detail/:id',
+    name: 'Detail',
+    component: Detail
+    }]
 })
